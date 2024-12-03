ac25c43a908644738ffc3673fdc68197dd078901
Sequencer <45060278+sequencerr@users.noreply.github.com>
2024-12-03 11:31:51 +0100

    add-note

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ import zopfli from '@gfx/zopfli';
 
 process.on('uncaughtException', () => console.log('not stepped into here'));
 
+// spitted whole code of library at the same throwing
 throw new Error();
